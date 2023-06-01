fc94b6f524446a7bf69dc7f94e020ca92591a8f4
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-06-01 20:07:38 -0300

    🎨 Update main_s3_management

--- main_s3_management.py
+++ main_s3_management.py
@@ -7,6 +7,7 @@ Date: May/2023
 
 # import necessary packages
 import logging
+import boto3
 from decouple import config
 
 from components.create_s3_raw_folder import move_files_to_daily_directory
@@ -23,9 +24,27 @@ AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 REGION_NAME = config('REGION_NAME')
 
+# Create a session with AWS credentials
+session = boto3.Session(
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION_NAME
+)
+
+ # Create a client instance for S3
+s3_client = session.client('s3')
+
 
 if __name__ == "__main__":
+    # 1. Move data from staging to RAW
     logging.info('About to start moving the data from staging to raw bucket')
-    move_files_to_daily_directory(
-        BUCKET_NAME, SOURCE_DIRECTORY, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, REGION_NAME)
+
+    response = s3_client.list_objects_v2(Bucket=BUCKET_NAME, Prefix=SOURCE_DIRECTORY)
+    csv_files = [obj['Key'] for obj in response['Contents'] if obj['Key'].lower().endswith('.csv')]
+
+    for file in csv_files:
+        move_files_to_daily_directory(
+            BUCKET_NAME, file, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, REGION_NAME)
     logging.info('Finish moving the data from staging to raw bucket')
+
+    # 2. Move data from RAW to PROCESSED
