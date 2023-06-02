48864302b35d9be4a5fb8639313fa688bf5f3642
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-06-01 21:35:49 -0300

    🎨 Create the management for processed layer

--- components/create_s3_processed_folder.py
+++ components/create_s3_processed_folder.py
@@ -8,10 +8,65 @@ Date: May/2023
 
 # import necessary packages
 import boto3
+import os
 import logging
 import datetime
+import pandas as pd
 
 logging.basicConfig(
     level=logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
+
+
+def move_files_to_processed_layer(
+        bucket_name: str,
+        aws_access_key_id: str,
+        aws_secret_access_key: str,
+        region_name: str) -> None:
+    '''
+    Process data for the current day from raw layer and save it in the processed layer of the data lake.
+
+    :param bucket_name: (str) Name of the S3 bucket.
+    :param aws_access_key_id: (str) AWS access key ID.
+    :param aws_secret_access_key: (str) AWS secret access key.
+    :param region_name: (str) AWS region name.
+    '''
+    # Get the current date
+    current_date = datetime.datetime.now().strftime('%Y-%m-%d')
+
+    # Define the paths for the raw and processed layers
+    raw_directory = f'raw/brand-data/atletico/official_page_tweets/extracted_at={current_date}/official_page_tweets.csv'
+    processed_directory = f'processed/brand-data/atletico/official_page_tweets/extracted_at={current_date}/official_page_tweets.parquet'
+
+    # Create a session with AWS credentials
+    session = boto3.Session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=region_name
+    )
+
+    # Create a client instance for S3
+    s3_client = session.client('s3')
+
+    # Read the raw data from S3, selecting only desired columns
+    obj = s3_client.get_object(Bucket=bucket_name, Key=raw_directory)
+    raw_data = pd.read_csv(obj['Body'], usecols=['tweet_id', 'created_at', 'text', 'retweets', 'likes'])
+
+    # Perform data transformations
+    processed_data = raw_data # Placeholder for actual data transformations
+
+    # Create the 'tmp' directory if it doesn't exist
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+
+    # Save the processed data to Parquet format
+    processed_data.to_parquet(f'tmp/{current_date}_processed_data.parquet')
+
+    # Upload the Parquet file to S3
+    s3_client.upload_file(f'tmp/{current_date}_processed_data.parquet', bucket_name, processed_directory)
+
+    # Delete the temporary file
+    os.remove(f'tmp/{current_date}_processed_data.parquet')
+
+    print(f'Data for {current_date} processed and saved in {processed_directory}.')
--- main_s3_management.py
+++ main_s3_management.py
@@ -12,6 +12,8 @@ from decouple import config
 
 from components.create_s3_raw_folder import move_files_to_raw_layer
 
+from components.create_s3_processed_folder import move_files_to_processed_layer
+
 logging.basicConfig(
     level=logging.INFO,
     filemode='w',
@@ -38,13 +40,18 @@ s3_client = session.client('s3')
 if __name__ == "__main__":
     # 1. Move data from staging to RAW
     logging.info('About to start moving the data from staging to raw bucket')
+    try:
+        response = s3_client.list_objects_v2(Bucket=BUCKET_NAME, Prefix=SOURCE_DIRECTORY)
+        csv_files = [obj['Key'] for obj in response['Contents'] if obj['Key'].lower().endswith('.csv')]
 
-    response = s3_client.list_objects_v2(Bucket=BUCKET_NAME, Prefix=SOURCE_DIRECTORY)
-    csv_files = [obj['Key'] for obj in response['Contents'] if obj['Key'].lower().endswith('.csv')]
-
-    for file in csv_files:
-        move_files_to_raw_layer(
-            BUCKET_NAME, file, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, REGION_NAME)
-    logging.info('Finish moving the data from staging to raw bucket')
+        for file in csv_files:
+            move_files_to_raw_layer(
+                BUCKET_NAME, file, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, REGION_NAME)
+    except:
+        pass
+    logging.info('Finish moving the data from staging to raw bucket\n')
 
     # 2. Move data from RAW to PROCESSED
+    logging.info('About to start moving the data from raw to processed layer')
+    move_files_to_processed_layer(BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, REGION_NAME)
+    logging.info('Finish moving the data from raw to processed layer\n')
