9947cdc1f1febf9b3e208a8d7f446d546f64a1b1
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-06-05 18:13:25 -0300

    🎨 Update curated folder funcs

--- components/create_s3_curated_folder.py
+++ components/create_s3_curated_folder.py
@@ -12,6 +12,92 @@ import logging
 import datetime
 import boto3
 import pandas as pd
+import re
+import string
+
+import nltk
+from nltk import ngrams
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from collections import Counter
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+
+def extract_ngrams(text, n):
+    '''
+    Extracts n-grams from a given text.
+
+    Args:
+        text (str): The text to extract n-grams from.
+        n (int): The size of the n-grams.
+
+    Returns:
+        list: A list of n-grams extracted from the text.
+    '''
+    tokens = word_tokenize(text.lower())  # Tokenization
+    stop_words = set(stopwords.words('portuguese'))
+    filtered_tokens = [token for token in tokens if token.isalpha() and token not in stop_words]  # Remove stopwords
+    
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]  # Lemmatize tokens
+    
+    n_grams = ngrams(lemmatized_tokens, n)  # Extract n-grams
+    return [' '.join(grams) for grams in n_grams]
+
+
+def clean_text(text):
+    '''
+    Cleans the text by converting it to lowercase, removing square brackets, links, punctuation,
+    and words containing numbers.
+
+    Args:
+        text (str): The text to be cleaned.
+
+    Returns:
+        str: The cleaned text.
+    '''
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text) # Remove the urls
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text) # Remove the punctuation
+    text = re.sub('\n', '',text)
+    text = re.sub('\w*\d\w*', '', text)
+    return text
+
+
+def get_ngram_frequencies(df, column, n):
+    '''
+    Extracts n-grams from a column of a DataFrame and calculates their frequencies.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the text column.
+        column (str): The name of the text column in the DataFrame.
+        n (int): The size of the n-grams.
+
+    Returns:
+        pandas.DataFrame: A DataFrame with columns for n-grams and their frequencies.
+    '''
+    ngrams_freq = Counter()
+    for text in df[column]:
+        cleaned_text = clean_text(text)
+        ngrams = extract_ngrams(cleaned_text, n)
+        ngrams_freq.update(ngrams)
+    
+    ngrams_list = []
+    freq_list = []
+    for ngram, freq in ngrams_freq.items():
+        ngrams_list.append(ngram)
+        freq_list.append(freq)
+    
+    new_df = pd.DataFrame()
+    new_df[f'ngrams_{n}'] = ngrams_list
+    new_df[f'frequencies_{n}'] = freq_list
+    
+    return new_df
 
 
 def move_files_to_curated_layer(
@@ -54,18 +140,37 @@ def move_files_to_curated_layer(
     # Perform additional data transformations
     curated_data = processed_data.copy()
 
+    curated_data['year'] = curated_data.created_at.dt.year
+    curated_data['month'] = curated_data.created_at.dt.month
+    curated_data['day'] = curated_data.created_at.dt.day
+    curated_data['hour'] = curated_data.created_at.dt.hour
+    curated_data['day_of_week'] = curated_data.created_at.dt.dayofweek
+
+    ngram_one = get_ngram_frequencies(curated_data, 'text', 1)
+    ngram_two = get_ngram_frequencies(curated_data, 'text', 2)
+    ngram_three = get_ngram_frequencies(curated_data, 'text', 3)
+
     # Create the 'tmp' directory if it doesn't exist
     if not os.path.exists('tmp'):
         os.makedirs('tmp')
 
     # Save the curated data to parquet format
     curated_data.to_parquet(f'tmp/{current_date}_curated_data.parquet', index=False)
+    ngram_one.to_parquet(f'tmp/{current_date}_ngram_one.parquet', index=False)
+    ngram_two.to_parquet(f'tmp/{current_date}_ngram_two.parquet', index=False)
+    ngram_three.to_parquet(f'tmp/{current_date}_ngram_three.parquet', index=False)
 
     # Upload the parquet file to S3
     s3_client.upload_file(f'tmp/{current_date}_curated_data.parquet', bucket_name, curated_directory)
+    s3_client.upload_file(f'tmp/{current_date}_ngram_one.parquet', bucket_name, curated_directory)
+    s3_client.upload_file(f'tmp/{current_date}_ngram_two.parquet', bucket_name, curated_directory)
+    s3_client.upload_file(f'tmp/{current_date}_ngram_three.parquet', bucket_name, curated_directory)
 
     # Delete the temporary files
     os.remove('tmp/processed_data.parquet')
     os.remove(f'tmp/{current_date}_curated_data.parquet')
+    os.remove(f'tmp/{current_date}_ngram_one.parquet')
+    os.remove(f'tmp/{current_date}_ngram_two.parquet')
+    os.remove(f'tmp/{current_date}_ngram_three.parquet')
 
     logging.info(f'Curated data for {current_date} processed and saved in {curated_directory}.')
