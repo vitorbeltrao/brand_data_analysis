fb473bb05b684f4b170744b9c4f1add9843492e8
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-06-24 13:07:41 -0300

    🎨 Finish component of create processed layer

--- components/create_s3_processed_folder.py
+++ components/create_s3_processed_folder.py
@@ -51,23 +51,60 @@ def move_files_to_processed_layer(
 
     # Read the raw data from S3, selecting only desired columns
     obj = s3_client.get_object(Bucket=bucket_name, Key=raw_directory)
-    raw_data = pd.read_csv(obj['Body'], names=['tweet_id', 'created_at', 'text', 'retweets', 'likes', 'id', 'ran_at', 'updated_at'])
+    raw_data = pd.read_csv(obj['Body'], names=['links', 'id', 'neo_reference_id', 'name', 'nasa_jpl_url', 
+                                               'absolute_magnitude_h', 'estimated_diameter', 'is_potentially_hazardous_asteroid',
+                                               'close_approach_data', 'is_sentry_object', 'created_at', 'updated_at'])
 
-    # Perform data transformations
+    ######################### Perform data transformations #########################
     processed_data = raw_data.copy()
-    processed_data['created_at'] = pd.to_datetime(processed_data['created_at'])
+    processed_data.drop(['links', 'neo_reference_id', 'nasa_jpl_url'], axis=1, inplace=True) # drop unnecessary columns
+    processed_data['name'] = processed_data['name'].str.replace(r'\(|\)', '', regex=True) # remove unnecessary parentesis of instances
+    logging.info(f'Columns have been removed: SUCCESS')
 
+    # normalize 'estimated_diameter' column
+    diameter = pd.json_normalize(processed_data['estimated_diameter'])
+    diameter = diameter[['kilometers.estimated_diameter_min', 'kilometers.estimated_diameter_max']]
+    logging.info(f'Column "estimated_diameter" have been normalized: SUCCESS')
+
+    # normalize 'close_approach_data' column
+    close_approach_slice_one = pd.DataFrame()
+
+    for item in processed_data['close_approach_data']:
+        df_item = pd.DataFrame(item, index=[0]) # Creates a DataFrame for each list item
+        close_approach_slice_one = pd.concat([close_approach_slice_one, df_item], ignore_index=True) # Concatenates the DataFrame to the result
+
+    close_approach_slice_two = pd.json_normalize(close_approach_slice_one['relative_velocity'])
+    close_approach_slice_three = pd.json_normalize(close_approach_slice_one['miss_distance'])
+
+    slice_one_two = close_approach_slice_one.join(close_approach_slice_two)
+    close_approach_final = slice_one_two.join(close_approach_slice_three)
+    close_approach_final = close_approach_final[['close_approach_date', 'orbiting_body', 
+                                                'kilometers_per_hour', 'kilometers']]
+    close_approach_final.rename(columns = {'kilometers_per_hour':'velocity_kilometers_per_hour',
+                                        'kilometers':'distance_kilometers'}, inplace = True)
+    logging.info(f'Column "close_approach_data" have been normalized: SUCCESS')
+    
+    # join everything in one final dataframe and do final modifications
+    processed_data_intermediate = processed_data.join(diameter)
+    processed_data_final = processed_data_intermediate.join(close_approach_final)
+    processed_data_final.drop(['estimated_diameter', 'close_approach_data'], axis=1, inplace=True)
+
+    processed_data_final['close_approach_date'] = pd.to_datetime(processed_data_final['close_approach_date'])
+    processed_data_final['velocity_kilometers_per_hour'] = processed_data_final['velocity_kilometers_per_hour'].astype(float)
+    processed_data_final['distance_kilometers'] = processed_data_final['distance_kilometers'].astype(float)
+
+    logging.info(f'All dataframes with transformations have been merged: SUCCESS')
+    ####################################################################################################
     # Create the 'tmp' directory if it doesn't exist
     if not os.path.exists('tmp'):
         os.makedirs('tmp')
 
     # Save the processed data to Parquet format
-    processed_data.to_parquet(f'tmp/{current_date}_processed_data.parquet')
+    processed_data.to_parquet(f'tmp/{current_date}_processed_asteroidsNeows.parquet')
 
     # Upload the Parquet file to S3
-    s3_client.upload_file(f'tmp/{current_date}_processed_data.parquet', bucket_name, processed_directory)
+    s3_client.upload_file(f'tmp/{current_date}_processed_asteroidsNeows.parquet', bucket_name, processed_directory)
 
     # Delete the temporary file
-    os.remove(f'tmp/{current_date}_processed_data.parquet')
-
+    os.remove(f'tmp/{current_date}_processed_asteroidsNeows.parquet')
     logging.info(f'Processed data for {current_date} processed and saved in {processed_directory}.')
